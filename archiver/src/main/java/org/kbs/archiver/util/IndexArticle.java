76d0ba50f1cb8c5b73dba28d3471ca3471baeb47
kcn <kcn@95af091c-fc56-470f-b468-855d73d27dd1>
2012-01-11 10:54:31 +0000

    try add lucene
    git-svn-id: file:///tmp/smth@94 95af091c-fc56-470f-b468-855d73d27dd1

---
+++ archiver/src/main/java/org/kbs/archiver/util/IndexArticle.java
@@ -0,0 +1,108 @@
+package org.kbs.archiver.util;
+
+//import org.apache.lucene.analysis.stard.StardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermEnum;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.util.Version;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Properties;
+import java.sql.*;
+
+import javax.sql.*;
+
+public class IndexArticle {
+
+	private static ClassPathXmlApplicationContext appContext;
+
+	public static void init() {
+		appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		appContext.registerShutdownHook();
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 先index标题吧
+		init();
+		DataSource ds = (DataSource) appContext.getBean("dataSource");
+		try {
+			Properties config = (Properties) appContext
+					.getBean("configproperties");
+			String workdir = ".";
+			if (config.get("workdir") != null) {
+				workdir = config.get("workdir") + "/lucene";
+			}
+			File index = new File(workdir);
+			Analyzer analyzer = new IKAnalyzer();// 采用的分词器
+			IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35,
+					analyzer);
+			conf.setMaxBufferedDocs(10240);
+			conf.setOpenMode(OpenMode.CREATE);
+			int nThreads = 0;
+			if (config.get("workerthreads") != null)
+				nThreads = Integer.parseInt((String) config
+						.get("workerthreads"));
+			if (nThreads <= 0)
+				nThreads = 4;
+			conf.setMaxThreadStates(nThreads);
+			IndexWriter writer = new IndexWriter(FSDirectory.open(index), conf);
+			long startTime = new Date().getTime();
+
+			Connection connection = ds.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement
+					.executeQuery("select articleid,subject from article");
+			while (rs.next()) {
+				Document doc = new Document();
+				doc.add(new Field("subject", rs.getString("subject"),
+						Field.Store.YES, Field.Index.ANALYZED));
+				doc.add(new Field("articleid", rs.getString("articleid"),
+						Field.Store.YES, Field.Index.NOT_ANALYZED));
+				writer.addDocument(doc);
+			}
+			//writer.optimize();
+			writer.close();
+			long endTime = new Date().getTime();
+			System.out.println("共建索引数：" + writer.numDocs());
+			System.out.println("时间：" + (endTime - startTime) + " 毫秒");
+			rs.close();
+			connection.close();
+		} catch (SQLException e) {
+			System.err.println("数据库操作失败：");
+			e.printStackTrace();
+		} catch (CorruptIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LockObtainFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
