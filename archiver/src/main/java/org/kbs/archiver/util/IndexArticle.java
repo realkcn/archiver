e5e23844def2a3fb27b3fcab080ffa007f100e6a
kcn <kcn@95af091c-fc56-470f-b468-855d73d27dd1>
2012-01-12 07:52:15 +0000

    git-svn-id: file:///tmp/smth@109 95af091c-fc56-470f-b468-855d73d27dd1

--- archiver/src/main/java/org/kbs/archiver/util/IndexArticle.java
+++ archiver/src/main/java/org/kbs/archiver/util/IndexArticle.java
@@ -17,6 +17,7 @@ import org.apache.lucene.util.Version;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LimitTokenCountAnalyzer;
 import org.kbs.archiver.lucene.Tools;
+import org.kbs.archiver.*;
 import org.kbs.archiver.persistence.ArticleBodyMapper;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.wltea.analyzer.lucene.IKAnalyzer;
@@ -28,10 +29,62 @@ import java.io.Reader;
 import java.util.Date;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.*;
 import java.sql.*;
 
 import javax.sql.*;
 
+class IndexThread implements Runnable {
+
+	private BlockingQueue<ArticleEntity> workqueue;
+	private Document document;
+	private Field articleidField;
+	private Field bodyField;
+	private IndexWriter writer;
+
+	public IndexThread(IndexWriter writer,BlockingQueue<ArticleEntity> queue) {
+		this.workqueue = queue;
+		this.writer=writer;
+		document=new Document();
+		articleidField=new Field("articleid","",Field.Store.YES, Field.Index.NO);
+		bodyField=new Field("body","",Field.Store.NO, Field.Index.ANALYZED);
+	}
+
+	public void add(ArticleEntity article) {
+		try {
+			workqueue.put(article);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (!Thread.interrupted()) {
+				ArticleEntity article = workqueue.take();
+				if (article.getArticleid()==-1)
+					break;
+				articleidField.setValue(new Long(article.getArticleid()).toString());
+				document.add(articleidField);
+				document.add(bodyField);
+				writer.addDocument(document);
+				document.removeField("articleid");
+				document.removeField("body");
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CorruptIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
 public class IndexArticle {
 
 	private static ClassPathXmlApplicationContext appContext;
@@ -51,14 +104,17 @@ public class IndexArticle {
 		init();
 		DataSource ds = (DataSource) appContext.getBean("dataSource");
 		try {
-			Properties config = (Properties) appContext.getBean("configproperties");
-			ArticleBodyMapper articleBodyMapper=(ArticleBodyMapper) appContext.getBean("articleBodyMapper");
-			File index = new File(Tools.getLucenceDirectory(appContext));
+			Properties config = (Properties) appContext
+					.getBean("configproperties");
+			ArticleBodyMapper articleBodyMapper = (ArticleBodyMapper) appContext
+					.getBean("articleBodyMapper");
+			File index = new File(Tools.getLucenceDirectory(appContext)+"new");
 			Analyzer analyzer = new IKAnalyzer();// 采用的分词器
-			LimitTokenCountAnalyzer limitanalyzer=new LimitTokenCountAnalyzer(analyzer,1000);
+			LimitTokenCountAnalyzer limitanalyzer = new LimitTokenCountAnalyzer(
+					analyzer, 1000);
 			IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_35,
 					limitanalyzer);
-//			conf.setMaxBufferedDocs(10240);
+			// conf.setMaxBufferedDocs(10240);
 			conf.setOpenMode(OpenMode.CREATE);
 			int nThreads = 0;
 			if (config.get("workerthreads") != null)
@@ -66,35 +122,50 @@ public class IndexArticle {
 						.get("workerthreads"));
 			if (nThreads <= 0)
 				nThreads = 4;
-			conf.setMaxThreadStates(nThreads);
-//			conf.setMaxBufferedDocs(500);
-			
-			
+			//conf.setMaxThreadStates(nThreads);
+			// conf.setMaxBufferedDocs(500);
+
 			IndexWriter writer = new IndexWriter(FSDirectory.open(index), conf);
-			//writer.setMaxFieldLength(200);
+			// writer.setMaxFieldLength(200);
 			long startTime = new Date().getTime();
 
 			Connection connection = ds.getConnection();
 			Statement statement = connection.createStatement();
 			ResultSet rs = statement
 					.executeQuery("select article.articleid,article.subject from article,board where board.boardid=article.boardid and board.ishidden=false and article.isvisible=true");
-			int count=0;
+			int count = 0;
+			Thread [] workerthread=new Thread[nThreads];
+			ArrayBlockingQueue<ArticleEntity> workqueue=new ArrayBlockingQueue<ArticleEntity>(1000);
+			for (int i=0;i<nThreads;i++) {
+				workerthread[i]=new Thread(new IndexThread(writer,workqueue));
+			}
 			while (rs.next()) {
 				Document doc = new Document();
 				count++;
-				doc.add(new Field("subject", rs.getString("subject"),
-						Field.Store.NO, Field.Index.ANALYZED));
-				String body=articleBodyMapper.get(rs.getLong("articleid"));
-				if (body!=null)
-					doc.add(new Field("body", body,
-							Field.Store.NO, Field.Index.ANALYZED));
-				doc.add(new Field("articleid", rs.getString("articleid"),
-						Field.Store.YES, Field.Index.NO));
-				writer.addDocument(doc);
-				if ((count%200000)==0)
-					System.out.println("索引文章 "+count+" 篇");
+				ArticleEntity article;
+				article=new ArticleEntity();
+				article.setArticleid(rs.getLong("articleid"));
+				String body = articleBodyMapper.get(rs.getLong("articleid"));
+				if (body != null)
+					article.setBody(body);
+				else {
+					System.err.println("Found article:"+article.getArticleid()+" without body");
+					article.setBody("");
+				}
+				workqueue.put(article);
+				if ((count % 200000) == 0)
+					System.out.println("索引文章 " + count + " 篇");
 			}
-			//writer.optimize();
+			//设置线程结束标志
+			ArticleEntity article=new ArticleEntity();
+			article.setArticleid(-1);
+			for (int i=0;i<nThreads;i++) {
+				workqueue.put(article);
+			}
+			for (int i=0;i<nThreads;i++) {
+				workerthread[i].join();
+			}
+			// writer.optimize();
 			long endTime = new Date().getTime();
 			System.out.println("文章数：" + count);
 			System.out.println("共建索引数：" + writer.numDocs());
@@ -110,6 +181,9 @@ public class IndexArticle {
 		} catch (LockObtainFailedException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
